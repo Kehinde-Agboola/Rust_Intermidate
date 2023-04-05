97a0e7992da4be152125365a83eca03d01a0e854
@kehindeoluseyi18 <kehindeoluseyi18@gmail.com>
2023-04-05 13:49:10 +0100

    Ownership

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+struct BankAccount{
+    balance: i32,
+    verified: bool
+}
+
+fn print_balance(account: &BankAccount){
+    println!("{:?}", account.balance)
+}
+fn print_verified(account: &BankAccount){
+    println!("{:?}", account.balance)
+}
+fn is_verified(account: &BankAccount){
+
+}
 fn main() {
-    println!("Hello, world!");
+    let my_account = BankAccount{
+        balance: 20,
+        verified: true
+    };
+    print_balance(&my_account);
+    print_verified(&my_account);
 }
